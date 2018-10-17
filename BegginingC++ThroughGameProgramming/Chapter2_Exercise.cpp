11c469cbe882feb97469d9b687815d2ab9045a0c
awoodcroft95 <awoodcroft95@gmail.com>
2018-10-17 15:54:01 +0100

    Added exercises for Chapter 2

---
+++ BegginingC++ThroughGameProgramming/Chapter2_Exercise.cpp
@@ -0,0 +1,64 @@
+#include "pch.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+void menuChooser()
+{
+	cout << "Difficulty Levels\n\n";
+	cout << "1 - Easy\n";
+	cout << "2 - Normal\n";
+	cout << "3 - Hard\n\n";
+
+	enum difficulty {EASY, NORMAL, HARD};
+
+	int intChoice;
+	difficulty choice;
+	cout << "Choice: ";
+	cin >> intChoice;
+
+	choice = (difficulty)(intChoice - 1);
+
+	switch (choice)
+	{
+	case EASY:
+		cout << "You picked Easy. \n";
+		break;
+	case NORMAL:
+		cout << "You picked Normal \n";
+		break;
+	case HARD:
+		cout << "You picked Hard \n";
+		break;
+	default:
+		cout << "You made an illegal choice. \n";
+		break;
+	} 
+}
+
+void guessMyNumber()
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+	string correct;
+	int count = 0;
+
+	cout << "Think of a random number between 1 and 100\n";
+	while (correct != "y")
+	{
+		int guess = rand() % 100 + 1;
+		cout << "Was this your number: (y/n) \n" << guess << "\n";
+		cin >> correct; 
+		count++;
+	}
+	cout << "I guessed it in" << count << "guesses";
+}
+
+int main()
+{
+	menuChooser();
+	guessMyNumber();
+	return 0;
+}
